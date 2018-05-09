883f2230a49a363bce9f0fa14f3ad830967c287e
Matias Gonzalez <matias.mart.gonzalez@gmail.com>
2018-05-09 15:45:29 -0300

    init commit

---
+++ translateApi.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request, abort
+from google.cloud import translate
+
+app = Flask(__name__)
+
+@app.route('/ZKTranslate/translate', methods=['POST'])
+def doTranslate():
+    content = request.json
+    print(content)
+    tc = translate.Client()
+    translations = []
+
+    for locale in content['target_language']:
+
+        translations.append('<data name="{}" xml:space="preserve">\
+                                <value>{}</value>\
+                            </data>'
+            .format(tc.translate(content['text'], target_language=locale)['translatedText'],
+                    content['text']))
+        data = {
+            'original_text': content['text'],
+            'translation': translations}
+        messages = None
+
+    return jsonify ({'errors': messages,'meta': None,'data':data})
